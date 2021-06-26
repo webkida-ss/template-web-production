3e93cf3580250d67db23e8f20927aa13d6078aff
webkida-ss <webkida.production@gmail.com>
2021-06-26 19:01:41 +0900

    js jQuery に修正

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(function () {
+jQuery(function () {
 
 	/*********************************************
 	 * WOW初期化
@@ -8,20 +8,20 @@ $(function () {
 	/*********************************************
 	 * スムーススクロール
 	 *********************************************/
-	$('a[href^="#"]').click(function () {
-		let header = $('.header').innerHeight();
+	jQuery('a[href^="#"]').click(function () {
+		let header = jQuery('.header').innerHeight();
 		let speed = 500;
-		let id = $(this).attr("href"); // 遷移先ID（href属性）
-		let target = $("#" == id ? "html" : id); // #のみだhtmlタグ（トップ）に戻る
-		let position = $(target).offset().top - header; // 固定ヘッダー分引く
+		let id = jQuery(this).attr("href"); // 遷移先ID（href属性）
+		let target = jQuery("#" == id ? "html" : id); // #のみだhtmlタグ（トップ）に戻る
+		let position = jQuery(target).offset().top - header; // 固定ヘッダー分引く
 		// ヘッダーがfixedでない場合
-		if ($(".header").css("position") !== "fixed") {
-			position = $(target).offset().top;
+		if (jQuery(".header").css("position") !== "fixed") {
+			position = jQuery(target).offset().top;
 		}
 		if (position < 0) {
 			position = 0;
 		}
-		$("html, body").animate({
+		jQuery("html, body").animate({
 				scrollTop: position
 			},
 			speed
@@ -32,11 +32,11 @@ $(function () {
 	/*********************************************
 	 * フローティングアイテム
 	 *********************************************/
-	$(window).on("scroll", function () {
-		if ($(this).scrollTop() > 200) {
-			$('.util__floating').addClass('is-show');
+	jQuery(window).on("scroll", function () {
+		if (jQuery(this).scrollTop() > 200) {
+			jQuery('.util__floating').addClass('is-show');
 		} else {
-			$('.util__floating').removeClass('is-show');
+			jQuery('.util__floating').removeClass('is-show');
 		}
 	});
 
@@ -44,22 +44,22 @@ $(function () {
 	 * ドロワー
 	 * jQuery
 	 *********************************************/
-	let drawerBtn = $("#js-drawer");
+	let drawerBtn = jQuery("#js-drawer");
 	drawerBtn.on("click", function (e) {
 		e.preventDefault();
-		let targetClass = $(this).attr("data-target");
-		$("." + targetClass).toggleClass("is-checked"); // for-drawerクラスがついてる要素をトグルでis-checked
+		let targetClass = jQuery(this).attr("data-target");
+		jQuery("." + targetClass).toggleClass("is-checked"); // for-drawerクラスがついてる要素をトグルでis-checked
 		return false;
 	});
 	// リンク選択時にドロワーを閉じる
-	$('.js-drawer-item').on('click', function (e) {
+	jQuery('.js-drawer-item').on('click', function (e) {
 		let targetClass = drawerBtn.attr("data-target");
-		$("." + targetClass).removeClass('is-checked');
+		jQuery("." + targetClass).removeClass('is-checked');
 	});
-	$(window).resize(function () {
-		if ($(window).width() > 559) {
+	jQuery(window).resize(function () {
+		if (jQuery(window).width() > 559) {
 			let targetClass = drawerBtn.attr("data-target");
-			$("." + targetClass).removeClass('is-checked');
+			jQuery("." + targetClass).removeClass('is-checked');
 		}
 	});
 
@@ -89,25 +89,25 @@ $(function () {
 	 * アコーディオン
 	 * jQuery
 	 *********************************************/
-	$('.js-accordion').click(function () {
+	jQuery('.js-accordion').click(function () {
 		// anwserの表示制御
-		$(this).children('.').next().slideToggle();
+		jQuery(this).children('.').next().slideToggle();
 		// 直下のquestion-textの表示制御
-		$(this).find('.').toggleClass('is-open');
+		jQuery(this).find('.').toggleClass('is-open');
 	});
 
 	/*********************************************
 	 * フォームバリデーション
 	 *********************************************/
-	let formName = $('#name'),
-		formKana = $('#name_kana'),
-		formPhone = $('#phone'),
-		formEmail = $('#email'),
-		formCreateRequest = $('#create-request'),
-		formRepairRequest = $('#repair-request'),
-		formOtherRequest = $('#other-request'),
-		formPrivacy = $('#privacy'),
-		formSubmit = $('#submit');
+	let formName = jQuery('#name'),
+		formKana = jQuery('#name_kana'),
+		formPhone = jQuery('#phone'),
+		formEmail = jQuery('#email'),
+		formCreateRequest = jQuery('#create-request'),
+		formRepairRequest = jQuery('#repair-request'),
+		formOtherRequest = jQuery('#other-request'),
+		formPrivacy = jQuery('#privacy'),
+		formSubmit = jQuery('#submit');
 	// チェックフラグ
 	let flgName = false,
 		flgKana = false,
@@ -116,10 +116,10 @@ $(function () {
 		flgRequest = false,
 		flgPrivacy = false;
 	// 正規表現
-	let regKana = /^([ァ-ン]|ー)+$/;
-	let regEmail = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/;
-	let regPhone = /^0[789]0[0-9]{4}[0-9]{4}$/;
-	let regPhoneHyphen = /^0[789]0-[0-9]{4}-[0-9]{4}$/;
+	let regKana = /^([ァ-ン]|ー)+jQuery/;
+	let regEmail = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}jQuery/;
+	let regPhone = /^0[789]0[0-9]{4}[0-9]{4}jQuery/;
+	let regPhoneHyphen = /^0[789]0-[0-9]{4}-[0-9]{4}jQuery/;
 	// チェック方針：アウトな場合trueを返す
 	let checkEmpty = str => (str == '' || str == null); // 空チェック
 	let checkName = name => checkEmpty(name); // 氏名チェック
