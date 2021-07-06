981bf27fa0dff7c4a151884195678bc8ef0369db
webkida-ss <webkida.production@gmail.com>
2021-07-06 20:09:00 +0900

    phpファイル追加

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php get_header(); ?>
+</head>
+
+<body>
+	<div class="wrapper">
+
+		<!-- ヘッダー-->
+		<?php get_template_part('src-php/_header'); ?>
+
+		<!-- メイン-->
+		<main class="main">
+
+
+
+
+
+
+
+		</main>
+
+		<!-- フッター-->
+		<?php get_template_part('src-php/_footer'); ?>
+
+	</div>
+	<?php get_footer(); ?>
+</body>
+
+</html>
