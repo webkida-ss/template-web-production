01a797a13d94887f4a3fce91621270358ae365b5
webkida-ss <webkida.production@gmail.com>
2021-07-10 13:14:26 +0900

    プラグイン追加

--- original-plugin/orginal-pluguin.php
+++ original-plugin/orginal-pluguin.php
@@ -4,6 +4,7 @@
  * https://www.udemy.com/course/wordpress_master/learn/lecture/22621816#overview
  * 格納先：plugins（themesと同階層）
  * テーマをまたいで共通的に使用したい機能はプラグイン化を推奨
+ * Plugin Nameは必須！
  */
 /**
  * Plugin Name: 自作プラグイン
@@ -11,3 +12,14 @@
  * Description: 自分専用プラグイン
  * Auther: Shun Sakakida
  */
+
+
+// 以下、functions.phpで記載するような内容を書いておくとよい！
+add_shortcode('hogehoge', function () {
+	return date('Y年 n月 j日');
+});
+
+function getPekePeke($param)
+{
+	return $param;
+}
