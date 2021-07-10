d1d72b647b7026a6cbce3041d270535d1977812f
webkida-ss <webkida.production@gmail.com>
2021-07-10 13:32:42 +0900

    functions修正

--- functions.php
+++ functions.php
@@ -3,7 +3,12 @@
 /************************************************************************
  * ファイル取込み
  ************************************************************************/
-get_template_part('src-functions/_admin_menu');
-get_template_part('src-functions/_after_setup_theme');
-get_template_part('src-functions/_wp_enqueue_scripts');
-get_template_part('src-functions/_user_define');
+
+// アクションフック
+get_template_part('src-functions/action-hook/_admin_menu');
+get_template_part('src-functions/action-hook/_after_setup_theme');
+get_template_part('src-functions/action-hook/_wp_enqueue_scripts');
+
+// その他
+get_template_part('src-functions/_user-define'); // ユーザ定義関数
+get_template_part('src-functions/_short-code');// ショートコード
