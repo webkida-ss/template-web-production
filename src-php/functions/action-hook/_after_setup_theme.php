e458dc8afa025b1c4a47973b8f74127e25cfab22
webkida-ss <webkida.production@gmail.com>
2021-07-10 18:37:24 +0900

    タイトルはWP管理画面から指定

--- src-php/functions/action-hook/_after_setup_theme.php
+++ src-php/functions/action-hook/_after_setup_theme.php
@@ -6,7 +6,7 @@
 add_action('after_setup_theme', function () {
 	add_theme_support('post-thumbnails'); // アイキャッチ画像を有効化
 	// add_theme_support('automatic-feed-links'); // 投稿とコメントのRSSフィードのリンクを有効化
-	// add_theme_support('title-tag'); // <title> タグが自動的に <head> タグ内に挿入される
+	add_theme_support('title-tag'); // 管理画面からのタイトルの設定（header.phpには直接記述しない）
 	// add_theme_support(
 	//   'html5',
 	//   array( //HTML5でマークアップ
