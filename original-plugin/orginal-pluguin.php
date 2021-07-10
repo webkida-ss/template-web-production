59a96080593d8a3a4bd305db0065dbd512ecccec
webkida-ss <webkida.production@gmail.com>
2021-07-10 13:07:25 +0900

    自作プラグインテンプレート作成

---
+++ original-plugin/orginal-pluguin.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * https://www.udemy.com/course/wordpress_master/learn/lecture/22621816#overview
+ * 格納先：plugins（themesと同階層）
+ * テーマをまたいで共通的に使用したい機能はプラグイン化を推奨
+ */
+/**
+ * Plugin Name: 自作プラグイン
+ * Version: 1.0
+ * Description: 自分専用プラグイン
+ * Auther: Shun Sakakida
+ */
