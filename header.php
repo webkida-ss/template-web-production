24bf875228c1762e443aa37b782210c445ada9a3
webkida-ss <webkida.production@gmail.com>
2021-07-10 20:25:59 +0900

    タイトルの出し分けはこれから

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="サイト説明サイト説明サイト説明">
 <!-- <meta name="robots" content="noindex"> -->
-<!-- <title>タイトルタイトルタイトル</title> -->
+<!-- <title><?php the_title(); ?></title> -->
 <!-- タイトルは管理画面から設定するためphpには直接記述しない：直接書くと書いた内容が表示される -->
 <link rel="icon" href="favicon.ico">
 
