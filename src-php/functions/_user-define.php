46f43faa1497fdb1e173aead87d5c83cc39839ba
webkida-ss <webkida.production@gmail.com>
2021-07-18 18:31:33 +0900

    ユーザ定義関数追加

--- src-php/functions/_user-define.php
+++ src-php/functions/_user-define.php
@@ -7,3 +7,22 @@ function getHogeHoge($param)
 {
 	return $param;
 }
+
+/*****************************************************
+ * 子ページ取得
+ * 呼び出し元で wp_reset_postdata すること
+ *****************************************************/
+function get_child_pages($number = -1)
+{
+	$parent_id = get_the_ID();
+	$args = array(
+		'posts_per_page' => $number, // 1ページに表示する最大投稿（-1：全部表示）
+		'post_type' => 'page', // 固定ページ
+		'orderby' => 'menu_order', // 並び順（管理画面）でソート
+		'order' => 'ASC',
+		'post_parent' => $parent_id,
+	);
+	// サブクエリ実行
+	$child_pages = new WP_Query($args);
+	return $child_pages;
+}
