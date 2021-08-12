a79f9f7b89ae10e5735dac8b49dc063d8a569dd1
webkida-ss <webkida.production@gmail.com>
2021-08-12 23:08:32 +0900

    修正

--- functions.php
+++ functions.php
@@ -5,13 +5,13 @@
  ************************************************************************/
 
 // アクションフック
-get_template_part('src-php/functions/action-hook/_admin_menu');
-get_template_part('src-php/functions/action-hook/_after_setup_theme');
-// get_template_part('src-php/functions/action-hook/_init');
-get_template_part('src-php/functions/action-hook/_widgets_init');
-get_template_part('src-php/functions/action-hook/_wp_enqueue_scripts');
+get_template_part('src-php/99_functions/action-hook/_admin_menu');
+get_template_part('src-php/99_functions/action-hook/_after_setup_theme');
+get_template_part('src-php/99_functions/action-hook/_init');
+get_template_part('src-php/99_functions/action-hook/_widgets_init');
+get_template_part('src-php/99_functions/action-hook/_wp_enqueue_scripts');
 
 // その他
-get_template_part('src-php/functions/_filter-hook'); // フィルターフック
-get_template_part('src-php/functions/_short-code'); // ショートコード
-get_template_part('src-php/functions/_user-define'); // ユーザ定義関数
+get_template_part('src-php/99_functions/_filter-hook'); // フィルターフック
+get_template_part('src-php/99_functions/_short-code'); // ショートコード
+get_template_part('src-php/99_functions/_user-define'); // ユーザ定義関数
