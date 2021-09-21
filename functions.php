b565e100ea400b6afde4801074824d29cce9f6ab
webkida-ss <webkida.production@gmail.com>
2021-09-21 20:41:31 +0900

    functions修正

--- functions.php
+++ functions.php
@@ -97,18 +97,20 @@ add_action('wp_enqueue_scripts', function () {
 
 /************************************************************************
  * セキュリティ対策：投稿者アーカイブの無効化
- * author_rewrite_rules
+ * author_rewrite_rules：投稿者アーカイブページのリライトルール
  * __return_empty_array：空の配列を返す
  ************************************************************************/
 add_filter('author_rewrite_rules', '__return_empty_array');
 function disable_author_archive()
 {
+	// 404へむりやりリダイレクト
 	if ($_GET['author'] || preg_match('#/author/.+#', $_SERVER['REQUEST_URI'])) {
 		wp_redirect(home_url('/404.php'));
 		exit;
 	}
 }
 add_action('init', 'disable_author_archive');
+
 
 /************************************************************************
  * ファイル取込み
