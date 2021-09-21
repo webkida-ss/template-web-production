d60ae8304d0b09b3321e6b04ecf2f6b3b076114c
webkida-ss <webkida.production@gmail.com>
2021-09-21 20:29:06 +0900

    セキュリティ対策追加

--- functions.php
+++ functions.php
@@ -95,6 +95,20 @@ add_action('wp_enqueue_scripts', function () {
 	wp_enqueue_script('script', get_template_directory_uri() . '/assets/js/script.min.js', array('jquery'), '1.0.0', true);
 });
 
+/************************************************************************
+ * セキュリティ対策：投稿者アーカイブの無効化
+ * author_rewrite_rules
+ * __return_empty_array：空の配列を返す
+ ************************************************************************/
+add_filter('author_rewrite_rules', '__return_empty_array');
+function disable_author_archive()
+{
+	if ($_GET['author'] || preg_match('#/author/.+#', $_SERVER['REQUEST_URI'])) {
+		wp_redirect(home_url('/404.php'));
+		exit;
+	}
+}
+add_action('init', 'disable_author_archive');
 
 /************************************************************************
  * ファイル取込み
