50a4920a6582791c7d0858dca9335c58a6d74591
webkida-ss <webkida.production@gmail.com>
2022-02-04 23:52:36 +0900

    W3C対策追加

--- functions.php
+++ functions.php
@@ -82,6 +82,20 @@ function disable_author_archive($query)
 add_action('parse_query', 'disable_author_archive');
 
 
+/************************************************************************
+ * W3C対策
+ * text/javascript削除
+ * text/css削除
+ ************************************************************************/
+add_action('template_redirect', function () {
+	ob_start(function ($buffer) {
+		$buffer = str_replace(array('type="text/javascript"', "type='text/javascript'"), '', $buffer);
+		$buffer = str_replace(array('type="text/css"', "type='text/css'"), '', $buffer);
+		return $buffer;
+	});
+});
+
+
 
 /************************************************************************
  * ファイル取込み
