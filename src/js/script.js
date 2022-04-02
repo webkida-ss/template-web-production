e72220d66824c271a5e091da0db308ad67672ede
webkida-ss <webkida.production@gmail.com>
2022-04-02 12:47:07 +0900

    script戻し

--- src/js/script.js
+++ src/js/script.js
@@ -36,6 +36,62 @@ jQuery(function () {
 	});
 
 	/*********************************************
+	 * ドロワー
+	 * jQuery
+	 *********************************************/
+	let drawerBtn = jQuery("#js-drawer");
+	drawerBtn.on("click", function (e) {
+		e.preventDefault();
+		let targetClass = jQuery(this).attr("data-target");
+		jQuery("." + targetClass).toggleClass("is-checked"); // for-drawerクラスがついてる要素をトグルでis-checked
+		return false;
+	});
+	// リンク選択時にドロワーを閉じる
+	jQuery('.js-drawer-item').on('click', function (e) {
+		let targetClass = drawerBtn.attr("data-target");
+		jQuery("." + targetClass).removeClass('is-checked');
+	});
+	jQuery(window).resize(function () {
+		if (jQuery(window).width() > 559) {
+			let targetClass = drawerBtn.attr("data-target");
+			jQuery("." + targetClass).removeClass('is-checked');
+		}
+	});
+
+	/*********************************************
+	 * スライダー
+	 * jQueryプラグイン
+	 *********************************************/
+	new Swiper('.swiper-container', {
+		width: 274, // スライドサイズ
+		spaceBetween: 24, // スライド間
+		loop: true, // 最後に達したら先頭に戻る
+		loopedSlides: 6, // スライド数
+		pagination: { // ページネーション
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+		},
+		breakpoints: { // ブレイクポイント
+			600: { // min-width 600
+				spaceBetween: 40,
+				width: 400,
+			}
+		}
+	});
+
+	/*********************************************
+	 * アコーディオン
+	 * jQuery
+	 *********************************************/
+	jQuery('.js-accordion').click(function () {
+		// anwserの表示制御
+		jQuery(this).children('.').next().slideToggle();
+		// 直下のquestion-textの表示制御
+		jQuery(this).find('.').toggleClass('is-open');
+	});
+
+	/*********************************************
 	 * スクロールトップ
 	 *********************************************/
 	jQuery('.js-scroll-top').click(function () {
