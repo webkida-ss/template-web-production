9701dafcf0f2bc2145c108090e0624b4539d1977
webkida-ss <webkida.production@gmail.com>
2022-04-03 11:20:52 +0900

    php header/footer修正

--- src-php/10_layout/_footer.php
+++ src-php/10_layout/_footer.php
@@ -1,3 +1,12 @@
 <footer class="l-footer">
-	<small class="l-footer__copy-right"></small>
-</footer><!-- /.l-footer -->
+	<div class="c-inner l-footer__inner">
+
+		<nav class="l-footer__nav">
+		</nav>
+
+		<small class="l-footer__copy-right">
+
+		</small>
+
+	</div><!-- /.l-footer__inner -->
+</footer>
